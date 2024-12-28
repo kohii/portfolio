13d4732eb14e9a3a0bf7d3b91d5aebd8e784cbb2
kohii <kohii.tokyo@gmail.com>
2024-12-28 11:27:59 +0900

    set custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://kohii.github.io/",
+	site: "https://kohii.dev/",
 	integrations: [tailwind()],
 });
